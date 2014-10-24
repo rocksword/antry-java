52e5381ffabbe487df92c83c2d164fc3bd8ea756
rocksword <rocksword@163.com>
2014-10-24 12:08:35 +0800

    String replace

--- src/main/java/com/an/antry/java/string/TestString.java
+++ src/main/java/com/an/antry/java/string/TestString.java
@@ -2,6 +2,12 @@ package com.an.antry.java.string;
 
 public class TestString {
     public static void main(String[] args) {
+        String line = "<th>aaa</th><th>bbb</th>lll";
+        String line2 = line.replace("/th>", "/th>\n");
+        System.out.println(line2);
+    }
+
+    private static void format() {
         String iv = String.format("%d", Integer.MAX_VALUE); // "2,147,483,647"
         System.out.println(iv);
 
